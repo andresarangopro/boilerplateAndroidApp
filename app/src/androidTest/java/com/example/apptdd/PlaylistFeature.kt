e86803f5cb7ccaab89ae5965ccf851e75252f1be
Andres Arango Rodriguez <a.arango@globant.com>
2021-08-25 12:39:57 -0500

    build the first instrument test

--- app/src/androidTest/java/com/example/apptdd/PlaylistFeature.kt
+++ app/src/androidTest/java/com/example/apptdd/PlaylistFeature.kt
@@ -2,11 +2,14 @@ package com.example.apptdd
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +17,13 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class PlaylistFeature {
+
+    val mActivityRule = ActivityTestRule(MainActivity::class.java)
+        @Rule get
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.apptdd", appContext.packageName)
+    fun displayScreenTitle() {
+       assertDisplayed("Playlists")
     }
 }
